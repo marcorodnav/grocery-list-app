33d8f450873a8e71b48a5cf110ef0a285e7f6284
Marco Rodríguez Navarro <marcorodnav@gmail.com>
2017-05-05 20:59:17 +0000

    Changes made by Marco Rodríguez Navarro (marcorodnav@gmail.com). Added registration functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,6 +42,28 @@
             );
         }
     });
+
+    window.registerView = kendo.observable({
+        submit: function() {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notificaiton.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, { Email: this.email },
+                function() {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function() {
+                    navigator.notification.alert("Unfortunately we were unable to create your account.");
+                }
+            );
+        }
+    });
     
     window.listView = kendo.observable({
         logout: function(event) {
